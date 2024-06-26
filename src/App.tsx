cf49e7e1997dc256e0070fe9a1ce4be3515d34ac
OsadchySa <osadchyalexandr0790@gmail.com>
2024-06-26 15:59:54 +0300

    hw1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-
+import React, {FC} from 'react';
 import './App.css';
-
-const App=()=>{
+import Car from "./Components/avtomobile/Car";
+const App: FC = () => {
   return (
-    <div></div>
+      <>
+          <Car nameOfClassImg={'img'} nameOfClassTitle={'title'} nameOfClassBut={'but'} model={'TESLA Model 3'} image={'https://upload.wikimedia.org/wikipedia/commons/9/91/2019_Tesla_Model_3_Performance_AWD_Front.jpg'}>RENT</Car>
+          <Car nameOfClassImg={'img'} nameOfClassTitle={'title'} nameOfClassBut={'but'} model={'TESLA Model Y'} image={'https://upload.wikimedia.org/wikipedia/commons/5/5c/Tesla_Model_Y_1X7A6211.jpg'}>RENT</Car>
+          <Car nameOfClassImg={'img'} nameOfClassTitle={'title'} nameOfClassBut={'but'} model={'TESLA Model X'} image={'https://upload.wikimedia.org/wikipedia/commons/c/c6/Tesla_Model_X_100D_1X7A6736.jpg'}>RENT</Car>
+      </>
   );
 }
 
---
+++ src/Components/avtomobile/Car.tsx
@@ -0,0 +1,32 @@
+import React, {FC} from 'react';
+import './car.css'
+
+interface IProps {
+    nameOfClassImg: string;
+    nameOfClassTitle: string;
+    nameOfClassBut: string;
+
+    model: string;
+    image: string;
+    children: React.ReactNode
+}
+const Car: FC<IProps> = ({nameOfClassImg, nameOfClassTitle, nameOfClassBut,
+                             model, image, children}) => {
+
+    return (
+        <div>
+
+            <button
+                className={nameOfClassBut}>
+
+                {children}
+            </button>
+            <h2 className={nameOfClassTitle}>{model}</h2>
+            <img className={nameOfClassImg} src={image}/>
+
+        </div>
+    )
+        ;
+};
+
+export default Car;
