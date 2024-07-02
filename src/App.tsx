42a0ace0e98605a8b6e7bcd63b2c753bc292934f
OsadchySa <osadchyalexandr0790@gmail.com>
2024-07-02 16:37:19 +0300

    hw3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App=()=>{
-  return (
-    <div></div>
-  );
+
+    const [value, setValue] = useState<string>('')
+    useEffect(()=>{setValue(value)})
+
+    const [counter, setCounter] = useState<number>(0)
+
+
+    return (
+        <div>
+            <div>
+                <h2>{value}</h2>
+                <button onClick={() => {
+                    setValue('TRUE')
+                }}>TRUE
+                </button>
+                <button onClick={() => {
+                    setValue('FALSE')
+                }}>FALSE
+                </button>
+            </div>
+            <div>
+                <button onClick={()=>{
+                    setCounter((prevState):any  => {
+                        console.log(prevState)
+                        return prevState + 1
+                    })
+                }}>click{counter}
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
