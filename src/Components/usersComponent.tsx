9e8fe6d59500cc7cec2f864627c7d727353f21a7
OsadchySa <osadchyalexandr0790@gmail.com>
2024-07-04 14:27:47 +0300

    hw4, 03.07, 14:27

---
+++ src/Components/usersComponent.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class UsersComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default UsersComponent;
