518a9a60d1a85954c8a70f7e394d950bd7493f1f
OsadchySa <osadchyalexandr0790@gmail.com>
2024-07-05 15:46:52 +0300

    hw4, 05.07, 15:46

--- src/Components/userComponent.tsx
+++ src/Components/userComponent.tsx
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
 import {IUser} from "../models/IUser";
+import {getPostsOfUserById} from "../services/api.service";
 
 type PropType = {
     user:IUser
 }
 class UserComponent extends Component <PropType>{
+    state = {
+        posts:[]
+    }
+    componentDidMount() {
+        getPostsOfUserById(this.props.user.id).then(response=>this.setState({posts:response.posts}))
+    }
 
     render() {
         return (
             <div>
                 {this.props.user.id}: {this.props.user.firstName} {this.props.user.lastName}
+                <button onClick={()=>{getPostsOfUserById(this.props.user.id).then(response=>console.log(response.posts))}}>GET POSTS</button>
             </div>
+
         );
     }
 }
