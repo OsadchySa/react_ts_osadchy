a13d1b547fed207ce143f7e269fa3a70b7ea703a
OsadchySa <osadchyalexandr0790@gmail.com>
2024-07-05 16:40:13 +0300

    hw4, 05.07, 16:40

--- src/Components/userComponent.tsx
+++ src/Components/userComponent.tsx
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 import {IUser} from "../models/IUser";
 import {getPostsOfUserById} from "../services/api.service";
 
+
 type PropType = {
     user:IUser
 }
+
 class UserComponent extends Component <PropType>{
     state = {
         posts:[]
@@ -17,7 +19,7 @@ class UserComponent extends Component <PropType>{
         return (
             <div>
                 {this.props.user.id}: {this.props.user.firstName} {this.props.user.lastName}
-                <button onClick={()=>{console.log(this.state.posts)}}>GET POSTS</button>
+                <button onClick={()=>{this.state.posts.map(value =>console.log(value))}}>GET POSTS</button>
             </div>
 
         );
