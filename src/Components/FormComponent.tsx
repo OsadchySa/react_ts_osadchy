d813ed85de18e36880237678caaa4c49a6fc2a64
OsadchySa <osadchyalexandr0790@gmail.com>
2024-07-07 20:37:32 +0300

    hw5, 07.07 20:37

--- src/Components/FormComponent.tsx
+++ src/Components/FormComponent.tsx
@@ -3,6 +3,11 @@ import {useForm} from "react-hook-form";
 import {joiResolver} from "@hookform/resolvers/joi";
 import userValidator from "../validators/userValidator";
 
+/*потрібно на jsonplaceholder відправити об'єкт post
+відповідно, для цього вам потрібно його створити. Створення об'єкту повинно відбуватись
+за допомоги форми з useForm та валідацією.
+Якщо ви не пам'ятаєте як відправляти данні на jsonplaceholder подивіться його документації*/
+
 type IFormType = {
     username:string,
     password:string,
