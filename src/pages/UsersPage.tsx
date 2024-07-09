744aaae2737fedd2e7818877bb776873627731b4
OsadchySa <osadchyalexandr0790@gmail.com>
2024-07-09 08:19:44 +0300

    hw6 09.07 8:20

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {getAllUsers} from "../services/api.service";
 import {IUser} from "../models/IUser";
 
 const UsersPage = () => {
     const [users, setUsers] = useState<IUser[]>()
-    getAllUsers().then(value => {setUsers(value)})
-    console.log(users)
+    useEffect(()=>{
+        getAllUsers().then((response) => {
+            setUsers([...response.users])
+        });
+    },[])
+
     return (
         <div>
             <UsersPage/>
