f38155c7c9ff6930d1cd005d9149cfe5cf27d38b
OsadchySa <osadchyalexandr0790@gmail.com>
2024-07-11 23:58:13 +0300

    hw6+ 11.07 23:58

---
+++ src/Component/button-components/NextButtonComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NextButtonComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NextButtonComponent;
---
+++ src/Component/button-components/PrevButtonComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrevButtonComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrevButtonComponent;
